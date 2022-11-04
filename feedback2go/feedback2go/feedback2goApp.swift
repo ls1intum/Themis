a980ba755e4c80d3069e5c3657acac59266ddc9a
Florian Huber <florian.maximilian.huber@tum.de>
2022-11-04 17:23:53 +0100

    initial empty app

---
+++ feedback2go/feedback2go/feedback2goApp.swift
@@ -0,0 +1,17 @@
+//
+//  feedback2goApp.swift
+//  feedback2go
+//
+//  Created by Florian Huber on 04.11.22.
+//
+
+import SwiftUI
+
+@main
+struct feedback2goApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
