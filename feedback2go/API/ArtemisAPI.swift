6c38121efba53b6ee623a4fe991b7a7a0773ea21
botrufus <64660684+botrufus@users.noreply.github.com>
2022-11-11 21:44:03 +0100

    Add API structure and example API call

--- feedback2go/API/ArtemisAPI.swift
+++ feedback2go/API/ArtemisAPI.swift
@@ -8,5 +8,31 @@
 import Foundation
 
 class ArtemisAPI {
+    static func sendRequest<T: Decodable, R: APIRequest>(_ type: T.Type, request: R) async throws -> T {
+        var request = request.request
+        
+        guard let token = Authentication.shared.token else {
+            throw Authentication.AuthenticationError.tokenNotFound
+        }
+        request.setBeaererToken(token: token)
+        
+        print(token)
+        
+        return try await RESTController.shared.sendRequest(request)
+    }
+    
+    static func sendRequest<T, R: APIRequest>(_ type: T.Type, request: R, decode: (Data) throws -> T) async throws -> T {
+        var request = request.request
+        
+        guard let token = Authentication.shared.token else {
+            throw Authentication.AuthenticationError.tokenNotFound
+        }
+        request.setBeaererToken(token: token)
+        
+        return try await RESTController.shared.sendRequest(request, decode: decode)
+    }
+}
 
+protocol APIRequest {
+    var request: Request { get }
 }
