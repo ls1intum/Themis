a5762350fb569978f8de349f60dce06152e40b9c
botrufus <64660684+botrufus@users.noreply.github.com>
2022-11-11 22:00:25 +0100

    delete print statement in ArtemisAPI and add Course.swift to project file

--- feedback2go/API/Course.swift
+++ feedback2go/API/Course.swift
@@ -6,14 +6,3 @@
 //
 
 import Foundation
-
-struct CourseTitle: Codable {
-    var title: String
-}
-
-struct GetCourseTitle: APIRequest {
-    let courseID: String
-    var request: Request {
-        Request(method: .get, path: "/api/courses/\(courseID)")
-    }
-}
