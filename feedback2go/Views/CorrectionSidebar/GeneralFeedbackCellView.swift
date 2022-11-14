911069000f2a0d8edf8779e915f1530c81b155a9
Florian Huber <florian.maximilian.huber@tum.de>
2022-11-14 22:43:54 +0100

    add picker and mock data

---
+++ feedback2go/Views/CorrectionSidebar/GeneralFeedbackCellView.swift
@@ -0,0 +1,20 @@
+//
+//  GeneralFeedbackView.swift
+//  feedback2go
+//
+//  Created by Florian Huber on 13.11.22.
+//
+
+import SwiftUI
+
+struct GeneralFeedbackCellView: View {
+    var body: some View {
+        Text("General Feedback").font(.headline)
+    }
+}
+
+struct GeneralFeedbackCellView_Previews: PreviewProvider {
+    static var previews: some View {
+        GeneralFeedbackCellView()
+    }
+}
