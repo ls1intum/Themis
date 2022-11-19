af1f1b723c00a0753f467d3494fbd40dc692ba60
Tom Rudnick <tom@rudnick.ch>
2022-11-19 12:37:28 +0100

    fix warnings

--- feedback2go/API/Assessment.swift
+++ feedback2go/API/Assessment.swift
@@ -12,12 +12,12 @@ extension ArtemisAPI {
     /// delete all saved feedback and release the lock of the submission
     static func cancelAssessment(submissionId: Int) async throws {
         let request = Request(method: .put, path: "/api/programming-submissions/\(submissionId)/cancel-assessment")
-        try await sendRequest(String.self, request: request)
+        let _ = try await sendRequest(String.self, request: request)
     }
 
     /// save feedback to the submission
     static func saveAssessment(participationId: Int, newAssessment: String) async throws {
         let request = Request(method: .post, path: "/participations/\(participationId)/manual-results", body: newAssessment)
-        try await sendRequest(String.self, request: request)
+        let _ = try await sendRequest(String.self, request: request)
     }
 }
