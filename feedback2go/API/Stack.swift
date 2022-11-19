2cbb9b281f6f399d0986cb8d16737a1a224e451e
Tom Rudnick <tom@rudnick.ch>
2022-11-19 17:02:39 +0100

    Added Fileparser

---
+++ feedback2go/API/Stack.swift
@@ -0,0 +1,43 @@
+//
+//  Stack.swift
+//  feedback2go
+//
+//  Created by Tom Rudnick on 19.11.22.
+//
+
+import Foundation
+
+
+protocol Stackable {
+    associatedtype Element
+    func peek() -> Element?
+    mutating func push(_ element: Element)
+    @discardableResult mutating func pop() -> Element?
+    func size() -> Int
+}
+
+extension Stackable {
+    var isEmpty: Bool { peek() == nil }
+}
+
+
+class Stack<Element>: ExpressibleByArrayLiteral, Stackable where Element: Equatable {
+    private var storage = [Element]()
+    func peek() -> Element? { storage.last }
+    func push(_ element: Element) { storage.append(element)  }
+    func pop() -> Element? { storage.popLast() }
+    func size() -> Int { storage.count }
+    
+    init() {
+        self.storage = []
+    }
+    
+    init(storage: [Element]) {
+        self.storage = storage
+    }
+    
+    required init(arrayLiteral elements: Stack.Element...) {
+        self.storage = elements
+    }
+
+}
