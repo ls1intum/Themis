2ed43a72cd3de33c469086020c3252634ae7d314
Florian Huber <florian.maximilian.huber@tum.de>
2022-11-20 17:07:48 +0100

    swiftlint fix

--- feedback2go/Views/CorrectionSidebar/Feedback.swift
+++ feedback2go/Views/CorrectionSidebar/Feedback.swift
@@ -12,7 +12,7 @@ public class Feedback: Identifiable {
     public var id: UUID
     public var feedbackText: String
     public var score: Double
-    
+
     init(id: UUID? = nil, feedbackText: String, score: Double) {
         self.id = id ?? UUID()
         self.feedbackText = feedbackText
