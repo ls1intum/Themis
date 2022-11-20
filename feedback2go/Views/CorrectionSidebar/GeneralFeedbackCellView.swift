0f883086d3e24640b2dfac5ab92b9f3dd4cc10c1
Florian Huber <florian.maximilian.huber@tum.de>
2022-11-20 17:01:50 +0100

    add possibility to give general feedback for submission

--- feedback2go/Views/CorrectionSidebar/GeneralFeedbackCellView.swift
+++ feedback2go/Views/CorrectionSidebar/GeneralFeedbackCellView.swift
@@ -8,13 +8,34 @@
 import SwiftUI
 
 struct GeneralFeedbackCellView: View {
+    @ObservedObject var feedbackModel: FeedbackViewModel
+    @State var showAddFeedback = false
+
     var body: some View {
-        Text("General Feedback").font(.headline)
+        VStack(alignment: .leading) {
+            HStack {
+                Text("General Feedback")
+                    .font(.largeTitle)
+                Spacer()
+                Button {
+                    showAddFeedback = true
+                } label: {
+                    Image(systemName: "plus")
+                }.font(.largeTitle)
+            }.padding()
+            List(feedbackModel.feedbacks) { feedback in
+                FeedbackCellView(feedbackModel: feedbackModel, feedbackID: feedback.id)
+            }
+            Spacer()
+        }.sheet(isPresented: $showAddFeedback) {
+            AddFeedbackView(feedbackModel: feedbackModel, showAddFeedback: $showAddFeedback)
+        }
     }
 }
 
 struct GeneralFeedbackCellView_Previews: PreviewProvider {
     static var previews: some View {
-        GeneralFeedbackCellView()
+        GeneralFeedbackCellView(feedbackModel: FeedbackViewModel.mock)
+            .previewInterfaceOrientation(.landscapeLeft)
     }
 }
