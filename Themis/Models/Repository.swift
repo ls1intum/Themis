289046d930e8742a086b6338ebdacacd1cc11396
Tom Rudnick <tom@rudnick.ch>
2022-11-25 12:38:10 +0100

    FlatMap for Filetree implemented

--- Themis/Models/Repository.swift
+++ Themis/Models/Repository.swift
@@ -44,6 +44,21 @@ class Node: Hashable {
     var path: String {
         calculatePath().joined(separator: "/")
     }
+    
+    ///This Method will flatMap children that have only one folder
+    func flatMap() {
+        guard let children, type == .folder else { return }
+        if let childFolder = children.first, childFolder.type == .folder, children.count == 1 {
+            self.name = self.name + "/" + childFolder.name
+            self.children?.removeFirst()
+            self.children = childFolder.children
+            self.flatMap()
+        } else {
+            for child in children {
+                child.flatMap()
+            }
+        }
+    }
 
     private func calculatePath() -> [String] {
         var parentPath = parent?.calculatePath() ?? []
@@ -124,7 +139,7 @@ extension ArtemisAPI {
         let timeInterval = Double(nanoTime) / 1_000_000_000
 
         print("Time to evaluate Parse: \(timeInterval) seconds")
-
+        root.flatMap()
         return root
     }
 
