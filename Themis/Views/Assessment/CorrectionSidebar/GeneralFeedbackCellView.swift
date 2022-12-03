48a16e017ec77eae2f64533022c8a75ee9c4faba
Evan Christopher <evanchristopher1907@gmail.com>
2022-12-04 00:05:04 +0100

    Categorized list for general and inline feedback

--- Themis/Views/Assessment/CorrectionSidebar/GeneralFeedbackCellView.swift
+++ Themis/Views/Assessment/CorrectionSidebar/GeneralFeedbackCellView.swift
@@ -12,6 +12,7 @@ struct GeneralFeedbackCellView: View {
     @EnvironmentObject var cvm: CodeEditorViewModel
 
     @State var showAddFeedback = false
+    @State var feedbackType: FeedbackType = .general
 
     var body: some View {
         VStack(alignment: .leading) {
@@ -21,21 +22,40 @@ struct GeneralFeedbackCellView: View {
                         .font(.title3)
                     Spacer()
                     Button {
-                        showAddFeedback = true
+                        feedbackType = .general
+                        showAddFeedback.toggle()
                     } label: {
                         Image(systemName: "plus")
                     }
                 }.padding()) {
-                    ForEach(assessment.feedback.feedbacks) { feedback in
+                    ForEach(assessment.feedback.feedbacks.filter { $0.type == .general }) { feedback in
+                        FeedbackCellView(feedback: feedback)
+                    }
+                    .onDelete(perform: delete(at:))
+                }.headerProminence(.increased)
+
+                Section(header: HStack {
+                    Text("Inline Feedback")
+                        .font(.title3)
+                    Spacer()
+                    Button {
+                        feedbackType = .inline
+                        showAddFeedback.toggle()
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                }.padding()) {
+                    ForEach(assessment.feedback.feedbacks.filter { $0.type == .inline }) { feedback in
                         FeedbackCellView(feedback: feedback)
                     }
                     .onDelete(perform: delete(at:))
                 }.headerProminence(.increased)
             }
             .listStyle(.sidebar)
+            .scrollContentBackground(.hidden)
             Spacer()
         }.sheet(isPresented: $showAddFeedback) {
-            EditFeedbackView(showEditFeedback: $showAddFeedback, feedback: nil, edit: false, type: .general)
+            EditFeedbackView(showEditFeedback: $showAddFeedback, feedback: nil, edit: false, type: feedbackType)
                 .environmentObject(assessment)
                 .environmentObject(cvm)
         }
