00a79f0eb69c71ad4e3f2b0050432a189c786e04
Paul Schwind <pal03377@users.noreply.github.com>
2022-12-12 23:02:45 +0100

    stop SwiftLint complaining

--- Themis/ViewModels/SubmissionList/SubmissionSearchResult.swift
+++ Themis/ViewModels/SubmissionList/SubmissionSearchResult.swift
@@ -12,10 +12,10 @@ private func levDis(_ w1: String, _ w2: String) -> Int {
     let empty = [Int](repeating: 0, count: w2.count)
     var last = [Int](0...w2.count)
 
-    for (i, char1) in w1.enumerated() {
-        var cur = [i + 1] + empty
-        for (j, char2) in w2.enumerated() {
-            cur[j + 1] = char1 == char2 ? last[j] : min(last[j], last[j + 1], cur[j]) + 1
+    for (index, char1) in w1.enumerated() {
+        var cur = [index + 1] + empty
+        for (index2, char2) in w2.enumerated() {
+            cur[index2 + 1] = char1 == char2 ? last[index2] : min(last[index2], last[index2 + 1], cur[index2]) + 1
         }
         last = cur
     }
