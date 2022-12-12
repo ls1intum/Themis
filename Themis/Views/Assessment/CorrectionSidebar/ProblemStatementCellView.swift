7822a0109b9611ee7f287dc87ba409d73f5e728a
Florian Huber <florian.maximilian.huber@tum.de>
2022-12-12 22:02:38 +0100

    show test overview bar in problem statement

--- Themis/Views/Assessment/CorrectionSidebar/ProblemStatementCellView.swift
+++ Themis/Views/Assessment/CorrectionSidebar/ProblemStatementCellView.swift
@@ -20,9 +20,35 @@ struct ProblemStatementCellView: View {
             Text("Problem Statement")
                 .font(.largeTitle)
 
+            ZStack {
+                Divider()
+                    .frame(height: 5)
+                    .overlay(.gray)
+                HStack {
+                    ForEach(vm.bundledTests, id: \.testCase) { bundledTest in
+                        if bundledTest != vm.bundledTests.first {
+                            Spacer()
+                        }
+                        if bundledTest.passed {
+                            Image("TestPassedSymbol")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(maxWidth: 30, maxHeight: 30)
+                                .background(colorScheme == .light ? .white : .black)
+                        } else {
+                            Image("TestFailedSymbol")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(maxWidth: 30, maxHeight: 30)
+                                .background(colorScheme == .light ? .white : .black)
+                        }
+                    }
+                }
+            }
+
             ForEach(vm.problemStatementParts, id: \.text) { part in
-                if let puml = part as? ProblemStatementPlantUML {
-                    AsyncImage(url: puml.url) { image in
+                if let uml = part as? ProblemStatementPlantUML {
+                    AsyncImage(url: uml.url) { image in
                         image.resizable()
                             .aspectRatio(contentMode: .fit)
                     } placeholder: {
