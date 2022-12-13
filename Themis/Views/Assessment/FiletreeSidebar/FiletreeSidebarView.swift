238ce8ee9855c595fb8ec8a0032033e64207faf0
Tom Rudnick <tom@rudnick.ch>
2022-12-13 20:47:42 +0100

    folders are now clickable

--- Themis/Views/Assessment/FiletreeSidebar/FiletreeSidebarView.swift
+++ Themis/Views/Assessment/FiletreeSidebar/FiletreeSidebarView.swift
@@ -3,36 +3,41 @@ import SwiftUI
 struct FiletreeSidebarView: View {
     @EnvironmentObject var assessmentViewModel: AssessmentViewModel
     @EnvironmentObject var cvm: CodeEditorViewModel
-
+    
     var body: some View {
         VStack(alignment: .leading) {
             Text("Filetree")
                 .font(.title)
                 .bold()
                 .padding(.leading, 18)
-            if let fileTree = cvm.fileTree {
-                List {
-                    OutlineGroup(fileTree, id: \.path, children: \.children) { tree in
-                        Text(tree.name)
-                            .bold(tree == cvm.selectedFile)
+            List {
+                OutlineGroup(cvm.fileTree, id: \.path, children: \.children) { tree in
+                    Group {
+                        if tree.type == .folder {
+                            Text(tree.name)
+                        } else {
+                            Button {
+                                withAnimation {
+                                    guard let pId = assessmentViewModel.submission?.participation.id else { return }
+                                    cvm.openFile(file: tree, participationId: pId)
+                                }
+                            } label: {
+                                Text(tree.name)
+                            }
+                            .buttonStyle(PlainButtonStyle())
                             .padding(.horizontal)
                             .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+                            .bold(tree === cvm.selectedFile)
                             .background(tree === cvm.selectedFile ? Color(UIColor.systemGray5) : Color(UIColor.systemBackground))
                             .cornerRadius(10)
-                            .tag(tree)
-                            .onTapGesture {
-                                if tree.type == .file {
-                                    withAnimation {
-                                        guard let pId = assessmentViewModel.submission?.participation.id else { return }
-                                        cvm.openFile(file: tree, participationId: pId)
-                                    }
-                                }
-                            }
+                        }
                     }
-                    .listRowSeparator(.hidden)
                 }
-                .listStyle(.inset)
+                .listRowSeparator(.hidden)
             }
+            .listStyle(.inset)
         }
     }
+    
+    
 }
