14560c4da800c24d7f11fa01a665adb2fda29407
botrufus <64660684+botrufus@users.noreply.github.com>
2022-12-14 14:58:49 +0100

    add extra automatic feedback section

--- Themis/Views/Assessment/CorrectionSidebar/GeneralFeedbackCellView.swift
+++ Themis/Views/Assessment/CorrectionSidebar/GeneralFeedbackCellView.swift
@@ -10,9 +10,9 @@ import SwiftUI
 struct GeneralFeedbackCellView: View {
     @EnvironmentObject var assessment: AssessmentViewModel
     @EnvironmentObject var cvm: CodeEditorViewModel
-
+    
     @State var showAddFeedback = false
-
+    
     var body: some View {
         VStack(alignment: .leading) {
             List {
@@ -32,7 +32,7 @@ struct GeneralFeedbackCellView: View {
                     }
                     .onDelete(perform: delete(at:))
                 }.headerProminence(.increased)
-
+                
                 Section {
                     ForEach(assessment.feedback.inlineFeedback) { feedback in
                         FeedbackCellView(feedback: feedback)
@@ -45,7 +45,19 @@ struct GeneralFeedbackCellView: View {
                         Spacer()
                     }.padding()
                 }.headerProminence(.increased)
+                Section {
+                    ForEach(assessment.feedback.automaticFeedback) { feedback in
+                        FeedbackCellView(feedback: feedback)
+                            .listRowSeparator(.hidden)
+                    }
+                } header: {
+                    HStack {
+                        Text("Automatic Feedback")
+                        Spacer()
+                    }.padding()
+                }.headerProminence(.increased)
             }
+            
             .listStyle(.sidebar)
             .scrollContentBackground(.hidden)
             Spacer()
@@ -55,7 +67,7 @@ struct GeneralFeedbackCellView: View {
                 .environmentObject(cvm)
         }
     }
-
+    
     private func delete(at indexSet: IndexSet) {
         indexSet
             .map { assessment.feedback.feedbacks[$0] }
@@ -69,7 +81,7 @@ struct GeneralFeedbackCellView: View {
 struct GeneralFeedbackCellView_Previews: PreviewProvider {
     static let assessment = AssessmentViewModel(readOnly: false)
     static let codeEditor = CodeEditorViewModel()
-
+    
     static var previews: some View {
         GeneralFeedbackCellView()
             .environmentObject(assessment)
