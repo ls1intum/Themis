bd207f18101f2546b7af55cd072dadd4de686332
Andreas Cselovszky <64660684+botrufus@users.noreply.github.com>
2023-01-12 11:17:56 +0100

    Add button to assess another random submission (#62)

--- Themis/Views/Assessment/AssessmentView.swift
+++ Themis/Views/Assessment/AssessmentView.swift
@@ -8,19 +8,20 @@ struct AssessmentView: View {
     @ObservedObject var vm: AssessmentViewModel
     @ObservedObject var cvm: CodeEditorViewModel
     @StateObject var umlVM = UMLViewModel()
-
+    
     @State var showSettings = false
     @State var showFileTree = true
     @State private var dragWidthLeft: CGFloat = UIScreen.main.bounds.size.width * 0.2
     @State private var dragWidthRight: CGFloat = 0
     @State private var correctionAsPlaceholder = true
     @State private var showCancelDialog = false
-
+    @State var showNoSubmissionsAlert = false
+    
     private let minRightSnapWidth: CGFloat = 185
-
+    
     let exerciseId: Int
     let exerciseTitle: String
-
+    
     var body: some View {
         ZStack(alignment: Alignment(horizontal: .leading, vertical: .top)) {
             HStack(spacing: 0) {
@@ -28,10 +29,11 @@ struct AssessmentView: View {
                     FiletreeSidebarView(
                         participationID: vm.submission?.participation.id,
                         cvm: cvm,
-                        templateParticipationId: vm.submission?.participation.exercise.templateParticipation.id ?? -1
+                        loading: vm.loading,
+                        templateParticipationId: vm.submission?.participation.exercise.templateParticipation?.id ?? -1
                     )
-                        .padding(.top, 35)
-                        .frame(width: dragWidthLeft)
+                    .padding(.top, 35)
+                    .frame(width: dragWidthLeft)
                     leftGrip
                         .edgesIgnoringSafeArea(.bottom)
                 }
@@ -39,7 +41,7 @@ struct AssessmentView: View {
                     cvm: cvm,
                     showFileTree: $showFileTree
                 )
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
                 rightGrip
                     .edgesIgnoringSafeArea(.bottom)
                 correctionWithPlaceholder
@@ -94,16 +96,16 @@ struct AssessmentView: View {
                             Task {
                                 if let pId = vm.submission?.participation.id {
                                     await vm.sendAssessment(participationId: pId, submit: false)
-                                    presentationMode.wrappedValue.dismiss()
                                 }
+                                presentationMode.wrappedValue.dismiss()
                             }
                         }
                         Button("Discard", role: .destructive) {
                             Task {
                                 if let id = vm.submission?.id {
                                     await vm.cancelAssessment(submissionId: id)
-                                    presentationMode.wrappedValue.dismiss()
                                 }
+                                presentationMode.wrappedValue.dismiss()
                             }
                         }
                     } message: {
@@ -124,6 +126,13 @@ struct AssessmentView: View {
                         .font(.title3)
                 }
                 .foregroundColor(.white)
+            }
+            if vm.loading {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    ProgressView()
+                        .frame(width: 20)
+                        .progressViewStyle(CircularProgressViewStyle(tint: Color.white))
+                }
             }
             if !vm.readOnly {
                 ToolbarItem(placement: .navigationBarTrailing) {
@@ -165,7 +174,7 @@ struct AssessmentView: View {
                     Text("Save")
                 }
                 .buttonStyle(NavigationBarButton())
-                .disabled(vm.readOnly)
+                .disabled(vm.readOnly || vm.loading)
             }
             ToolbarItem(placement: .navigationBarTrailing) {
                 Button {
@@ -178,7 +187,26 @@ struct AssessmentView: View {
                     Text("Submit")
                 }
                 .buttonStyle(NavigationBarButton())
-                .disabled(vm.readOnly)
+                .disabled(vm.readOnly || vm.loading)
+            }
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button {
+                    Task {
+                        await vm.initRandomSubmission(exerciseId: exerciseId)
+                        if vm.submission == nil {
+                            showNoSubmissionsAlert = true
+                        }
+                    }
+                } label: {
+                    Text("Next")
+                }
+                .buttonStyle(NavigationBarButton())
+                .disabled(vm.readOnly || vm.loading)
+            }
+        }
+        .alert("No more submissions to assess.", isPresented: $showNoSubmissionsAlert) {
+            Button("OK", role: .cancel) {
+                presentationMode.wrappedValue.dismiss()
             }
         }
         .sheet(isPresented: $showSettings) {
@@ -186,7 +214,7 @@ struct AssessmentView: View {
                 AppearanceSettingsView(
                     fontSize: $cvm.editorFontSize
                 )
-                    .navigationTitle("Appearance settings")
+                .navigationTitle("Appearance settings")
             }
         }
         .sheet(isPresented: $cvm.showAddFeedback) {
@@ -208,7 +236,7 @@ struct AssessmentView: View {
         ZStack {
             Color.primary
                 .frame(maxWidth: 7, maxHeight: .infinity)
-
+            
             Rectangle()
                 .opacity(0)
                 .frame(width: 20, height: 50)
@@ -272,7 +300,7 @@ struct AssessmentView: View {
                             let minWidth: CGFloat = 0
                             let screenWidth: CGFloat = UIScreen.main.bounds.size.width
                             let maxWidth: CGFloat = screenWidth * 0.3 > minRightSnapWidth ? screenWidth * 0.3 : 1.5 * minRightSnapWidth
-
+                            
                             let delta = gesture.translation.width
                             dragWidthRight -= delta
                             if dragWidthRight > maxWidth {
@@ -280,7 +308,7 @@ struct AssessmentView: View {
                             } else if dragWidthRight < minWidth {
                                 dragWidthRight = minWidth
                             }
-
+                            
                             correctionAsPlaceholder = dragWidthRight < minRightSnapWidth ? true : false
                         }
                         .onEnded {_ in
@@ -290,7 +318,7 @@ struct AssessmentView: View {
                         }
                 )
                 .zIndex(1)
-
+            
             if dragWidthRight > 0 {
                 Color.primary
                     .frame(maxWidth: 7, maxHeight: .infinity)
@@ -306,22 +334,26 @@ struct AssessmentView: View {
         .frame(width: 7)
     }
     var correctionWithPlaceholder: some View {
-        // TODO: ViewModifier for conditional redacted + remove redacted and remove text when to small as way to laggy
         VStack {
             if correctionAsPlaceholder {
                 EmptyView()
             } else {
                 CorrectionSidebarView(
+                    problemStatement: Binding(
+                        get: { vm.submission?.participation.exercise.problemStatement ?? "" },
+                        set: { vm.submission?.participation.exercise.problemStatement = $0 }
+                    ),
                     exercise: vm.submission?.participation.exercise,
                     readOnly: vm.readOnly,
                     assessmentResult: $vm.assessmentResult,
                     cvm: cvm,
-                    umlVM: umlVM
+                    umlVM: umlVM,
+                    loading: vm.loading
                 )
             }
         }
     }
-
+    
     var scoreColor: Color {
         guard let max = vm.submission?.participation.exercise.maxPoints else {
             return Color(.systemRed)
@@ -335,7 +367,7 @@ struct AssessmentView: View {
             return Color(.systemGreen)
         }
     }
-
+    
     var scoreDisplay: some View {
         Group {
             if let submission = vm.submission {
@@ -349,7 +381,7 @@ struct AssessmentView: View {
                          \(submission.participation.exercise.maxPoints
                          .formatted(FloatingPointFormatStyle()))
                          """)
-                        .foregroundColor(.white)
+                    .foregroundColor(.white)
                 }
             }
         }
@@ -361,13 +393,13 @@ extension Color {
     public static var primary: Color {
         Color("primary")
     }
-
+    
     public static var secondary: Color {
         Color("secondary")
     }
 }
 
-struct AssessmentView_Previews: PreviewProvider {
+ struct AssessmentView_Previews: PreviewProvider {
     static let avm = AssessmentViewModel(readOnly: false)
     static let cvm = CodeEditorViewModel()
 
@@ -380,16 +412,4 @@ struct AssessmentView_Previews: PreviewProvider {
         )
             .previewInterfaceOrientation(.landscapeLeft)
     }
-}
-
-struct NavigationBarButton: ButtonStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        configuration.label
-            .foregroundColor(.white)
-            .padding(.horizontal, 10)
-            .padding(.vertical, 5)
-            .background(Color.secondary)
-            .cornerRadius(20)
-            .fontWeight(.semibold)
-    }
-}
+ }
