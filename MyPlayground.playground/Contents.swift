ef4ee030f51f6c320dddcfd25102d40d7af2407b
Tom Rudnick <tom@rudnick.ch>
2023-01-23 10:58:30 +0100

    Added all icons by hand into assets folder

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,26 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+func shell(_ command: String) -> String {
+    let task = Process()
+    let pipe = Pipe()
+    
+    task.standardOutput = pipe
+    task.standardError = pipe
+    task.arguments = ["-c", command]
+    task.launchPath = "/bin/zsh"
+    task.standardInput = nil
+    task.launch()
+    
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    let output = String(data: data, encoding: .utf8)!
+    
+    return output
+}
+
+let s = shell("echo ls")
+
+
+print(s.trimmingCharacters(in: .whitespacesAndNewlines))
+print(2)
