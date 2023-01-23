4eed90f9f6ac4ca9d1e8b767986b70cdf42f5005
Tom Rudnick <tom@rudnick.ch>
2023-01-23 21:05:57 +0100

    build time improvements

--- Themis/Views/Assessment/FiletreeSidebar/FiletreeSidebarView.swift
+++ Themis/Views/Assessment/FiletreeSidebar/FiletreeSidebarView.swift
@@ -17,29 +17,9 @@ struct FiletreeSidebarView: View {
                 List {
                     OutlineGroup(cvm.fileTree, id: \.path, children: \.children) { tree in
                         if tree.type == .folder {
-                            HStack {
-                                Image(systemName: "folder")
-                                Text(tree.name)
-                            }
-                           
+                            nodeFolderView(folder: tree)
                         } else {
-                            Button {
-                                withAnimation {
-                                    guard let participationID else { return }
-                                    cvm.openFile(file: tree,
-                                             participationId: participationID,
-                                             templateParticipationId: templateParticipationId
-                                    )
-                                }
-                            } label: {
-                                FileView(file: tree)
-                            }
-                            .buttonStyle(PlainButtonStyle())
-                            .padding(.horizontal)
-                            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
-                            .bold(tree === cvm.selectedFile)
-                            .background(tree === cvm.selectedFile ? Color(UIColor.systemGray5) : Color(UIColor.systemBackground))
-                            .cornerRadius(10)
+                            nodeFileView(file: tree)
                         }
                     }
                     .listRowSeparator(.hidden)
@@ -49,6 +29,35 @@ struct FiletreeSidebarView: View {
             Spacer()
         }
         .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    @ViewBuilder
+    func nodeFolderView(folder: Node) -> some View{
+        HStack {
+            Image(systemName: "folder")
+            Text(folder.name)
+        }
+    }
+    
+    @ViewBuilder
+    func nodeFileView(file: Node) -> some View {
+        Button {
+            withAnimation {
+                guard let participationID else { return }
+                cvm.openFile(file: file,
+                         participationId: participationID,
+                         templateParticipationId: templateParticipationId
+                )
+            }
+        } label: {
+            FileView(file: file)
+        }
+        .buttonStyle(PlainButtonStyle())
+        .padding(.horizontal)
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+        .bold(file === cvm.selectedFile)
+        .background(file === cvm.selectedFile ? Color(UIColor.systemGray5) : .green)
+        .cornerRadius(10)
     }
 
 }
