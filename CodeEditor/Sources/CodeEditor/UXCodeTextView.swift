3362d78ae4cb6ef8312e90a77df287d1792f7a6b
Alexander Lange <alange01@gmx.de>
2023-01-31 23:14:05 +0100

    change request

--- CodeEditor/Sources/CodeEditor/UXCodeTextView.swift
+++ CodeEditor/Sources/CodeEditor/UXCodeTextView.swift
@@ -28,28 +28,28 @@ typealias UXTextViewDelegate  = UITextViewDelegate
  * Currently pretty tightly coupled to `CodeEditor`.
  */
 final class UXCodeTextView: UXTextView, HighlightDelegate, UIScrollViewDelegate {
-
+    
     fileprivate let highlightr = Highlightr()
-
+    
     private var hlTextStorage: CodeAttributedString? {
         textStorage as? CodeAttributedString
     }
     var customLayoutManager: RoundedCornerLayoutManager!
-
+    
     /// If the user starts a newline, the editor automagically adds the same
     /// whitespace as on the previous line.
     var isSmartIndentEnabled = true
-
+    
     var indentStyle          = CodeEditor.IndentStyle.system {
         didSet {
             guard oldValue != indentStyle else { return }
             reindent(oldStyle: oldValue)
         }
     }
-
+    
     var autoPairCompletion = [ String: String ]()
     var oldWidth: CGFloat = 0.0
-
+    
     var language: CodeEditor.Language? {
         set {
             guard hlTextStorage?.language != newValue?.rawValue else { return }
@@ -63,7 +63,7 @@ final class UXCodeTextView: UXTextView, HighlightDelegate, UIScrollViewDelegate 
             if let font = highlightr?.theme?.codeFont { self.font = font; self.customLayoutManager.lineNumberFont = font }
         }
     }
-
+    
     var highlightedRanges: [HighlightedRange] = []
     var dragSelection: Range<Int>?
     
@@ -96,7 +96,7 @@ final class UXCodeTextView: UXTextView, HighlightDelegate, UIScrollViewDelegate 
         let textContainer = NSTextContainer()
         textContainer.widthTracksTextView = true // those are key!
         layoutManager.addTextContainer(textContainer)
-
+        
         super.init(frame: .zero, textContainer: textContainer)
         if let hlTextStorage {
             hlTextStorage.highlightDelegate = self
@@ -106,7 +106,7 @@ final class UXCodeTextView: UXTextView, HighlightDelegate, UIScrollViewDelegate 
 #if os(macOS)
         isVerticallyResizable = true
         maxSize               = .init(width: 0, height: 1_000_000)
-
+        
         isRichText                           = false
         allowsImageEditing                   = false
         isGrammarCheckingEnabled             = false
@@ -123,50 +123,50 @@ final class UXCodeTextView: UXTextView, HighlightDelegate, UIScrollViewDelegate 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     // MARK: - Actions
-
+    
 #if os(macOS)
     override func changeColor(_ sender: Any?) {
         // https://github.com/ZeeZide/CodeEditor/issues/12
         // Reject user based color changes.
     }
-
+    
     override func changeFont(_ sender: Any?) {
         let coordinator = delegate as? UXCodeTextViewDelegate
-
+        
         let old = coordinator?.fontSize
         ?? highlightr?.theme?.codeFont?.pointSize
         ?? NSFont.systemFontSize
         let new: CGFloat
-
+        
         let fm = NSFontManager.shared
         switch fm.currentFontAction {
         case .sizeUpFontAction: new = old + 1
         case .sizeDownFontAction: new = old - 1
-
+            
         case .viaPanelFontAction:
             guard let font = fm.selectedFont else {
                 return super.changeFont(sender)
             }
             new = font.pointSize
-
+            
         case .addTraitFontAction, .removeTraitFontAction: // bold/italic
             NSSound.beep()
             return
-
+            
         default:
             guard let font = fm.selectedFont else {
                 return super.changeFont(sender)
             }
             new = font.pointSize
         }
-
+        
         coordinator?.fontSize = new
         applyNewFontSize(new)
     }
 #endif // macOS
-
+    
     override func copy(_ sender: Any?) {
         guard let coordinator = delegate as? UXCodeTextViewDelegate else {
             assertionFailure("Expected coordinator as delegate")
@@ -174,9 +174,9 @@ final class UXCodeTextView: UXTextView, HighlightDelegate, UIScrollViewDelegate 
         }
         if coordinator.allowCopy { super.copy(sender) }
     }
-
+    
     private var isAutoPairEnabled: Bool { !autoPairCompletion.isEmpty }
-
+    
 #if os(iOS)
     override func insertText(_ text: String) {
         super.insertText(text)
@@ -189,23 +189,23 @@ final class UXCodeTextView: UXTextView, HighlightDelegate, UIScrollViewDelegate 
 #endif
 #if os(macOS)
     // MARK: - Smarts as shown in https://github.com/naoty/NTYSmartTextView
-
+    
     override func insertNewline(_ sender: Any?) {
         guard isSmartIndentEnabled else { return super.insertNewline(sender) }
-
+        
         let currentLine = self.currentLine
         let wsPrefix = currentLine.prefix(while: {
             guard let scalar = $0.unicodeScalars.first else { return false }
             return CharacterSet.whitespaces.contains(scalar) // yes, yes
         })
-
+        
         super.insertNewline(sender)
-
+        
         if !wsPrefix.isEmpty {
             insertText(String(wsPrefix), replacementRange: selectedRange())
         }
     }
-
+    
     override func insertTab(_ sender: Any?) {
         guard case .softTab(let width) = indentStyle else {
             return super.insertTab(sender)
@@ -213,62 +213,62 @@ final class UXCodeTextView: UXTextView, HighlightDelegate, UIScrollViewDelegate 
         super.insertText(String(repeating: " ", count: width),
                          replacementRange: selectedRange())
     }
-
+    
     override func insertText(_ string: Any, replacementRange: NSRange) {
         super.insertText(string, replacementRange: replacementRange)
         guard isAutoPairEnabled              else { return }
         guard let string = string as? String else {
             return
         } // TBD: NSAttrString
-
+        
         guard let end = autoPairCompletion[string] else {
             return
         }
         super.insertText(end, replacementRange: selectedRange())
         super.moveBackward(self)
     }
-
+    
     override func deleteBackward(_ sender: Any?) {
         guard isAutoPairEnabled, !isStartOrEndOfLine else {
             return super.deleteBackward(sender)
         }
-
+        
         let s             = self.string
         let selectedRange = swiftSelectedRange
         guard selectedRange.lowerBound > s.startIndex,
               selectedRange.lowerBound < s.endIndex else {
             return super.deleteBackward(sender)
         }
-
+        
         let startIdx  = s.index(before: selectedRange.lowerBound)
         let startChar = s[startIdx..<selectedRange.lowerBound]
         guard let expectedEndChar = autoPairCompletion[String(startChar)] else {
             return super.deleteBackward(sender)
         }
-
+        
         let endIdx    = s.index(after: selectedRange.lowerBound)
         let endChar   = s[selectedRange.lowerBound..<endIdx]
         guard expectedEndChar[...] == endChar else {
             return super.deleteBackward(sender)
         }
-
+        
         super.deleteForward(sender)
         super.deleteBackward(sender)
     }
 #endif // macOS
-
+    
     private func reindent(oldStyle: CodeEditor.IndentStyle) {
         // - walk over the lines, strip and count the whitespaces and do something
         //   clever :-)
     }
-
+    
     // MARK: - Themes
-
+    
     @discardableResult
     func applyNewFontSize(_ newSize: CGFloat) -> Bool {
         applyNewTheme(nil, andFontSize: newSize)
     }
-
+    
     @discardableResult
     func applyNewTheme(_ newTheme: CodeEditor.ThemeName) -> Bool {
         guard themeName != newTheme else { return false }
@@ -279,7 +279,7 @@ final class UXCodeTextView: UXTextView, HighlightDelegate, UIScrollViewDelegate 
         themeName = newTheme
         return true
     }
-
+    
     @discardableResult
     func applyNewTheme(_ newTheme: CodeEditor.ThemeName? = nil,
                        andFontSize newSize: CGFloat) -> Bool {
@@ -287,12 +287,12 @@ final class UXCodeTextView: UXTextView, HighlightDelegate, UIScrollViewDelegate 
         guard let highlightr = highlightr,
               highlightr.setTheme(to: (newTheme ?? themeName).rawValue),
               let theme      = highlightr.theme else { return false }
-
+        
         if let font = theme.codeFont, font !== self.font { self.font = font }
         guard theme.codeFont?.pointSize != newSize else { return true }
-
-        theme.codeFont = theme.codeFont?      .withSize(newSize)
-        theme.boldCodeFont   = theme.boldCodeFont?  .withSize(newSize)
+        
+        theme.codeFont = theme.codeFont?.withSize(newSize)
+        theme.boldCodeFont = theme.boldCodeFont?.withSize(newSize)
         theme.italicCodeFont = theme.italicCodeFont?.withSize(newSize)
         if let newTheme {
             themeName = newTheme
@@ -302,8 +302,8 @@ final class UXCodeTextView: UXTextView, HighlightDelegate, UIScrollViewDelegate 
     
     func changeFontSize(size: CGFloat) {
         if let theme = highlightr?.theme {
-            theme.codeFont = theme.codeFont?      .withSize(size)
-            theme.boldCodeFont   = theme.boldCodeFont?  .withSize(size)
+            theme.codeFont = theme.codeFont?.withSize(size)
+            theme.boldCodeFont = theme.boldCodeFont? .withSize(size)
             theme.italicCodeFont = theme.italicCodeFont?.withSize(size)
         }
         font = font?.withSize(size)
@@ -316,7 +316,7 @@ final class UXCodeTextView: UXTextView, HighlightDelegate, UIScrollViewDelegate 
             oldWidth = self.bounds.width
         }
     }
-
+    
     private func numViewWidth() -> CGFloat {
         let maxNum = 4.0
         if let font {
@@ -327,7 +327,7 @@ final class UXCodeTextView: UXTextView, HighlightDelegate, UIScrollViewDelegate 
         }
         return 30
     }
-
+    
     override func draw(_ rect: CGRect) {
         let ctx = UIGraphicsGetCurrentContext()
         guard let ctx else { return }
@@ -353,7 +353,7 @@ final class UXCodeTextView: UXTextView, HighlightDelegate, UIScrollViewDelegate 
         UIGraphicsPopContext()
     }
     
-    func drawHiglights() {
+    func drawHighlights() {
         if !text.isEmpty {
             for hRange in highlightedRanges {
                 self.textStorage.addAttributes(
@@ -367,7 +367,7 @@ final class UXCodeTextView: UXTextView, HighlightDelegate, UIScrollViewDelegate 
             }
         }
     }
-
+    
     func didHighlight(_ range: NSRange, success: Bool) {
         if !text.isEmpty {
             for hRange in highlightedRanges {
@@ -382,13 +382,13 @@ final class UXCodeTextView: UXTextView, HighlightDelegate, UIScrollViewDelegate 
             }
         }
     }
-        
+    
     
     private func getGlyphIndex(textView: UXCodeTextView, point: CGPoint) -> Int {
         let point = CGPoint(x: point.x, y: point.y - (self.font?.pointSize ?? 0.0) / 2.0)
         return self.layoutManager.glyphIndex(for: point, in: textView.textContainer)
     }
-
+    
     private func getSelectionFromLine() -> Range<Int>? {
         guard let firstPoint, let secondPoint else { return nil }
         let firstGlyphIndex = getGlyphIndex(textView: self, point: firstPoint)
@@ -416,7 +416,7 @@ final class UXCodeTextView: UXTextView, HighlightDelegate, UIScrollViewDelegate 
             setNeedsDisplay()
         }
     }
-        
+    
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         let coordinator = delegate as? UXCodeTextViewDelegate
@@ -427,14 +427,14 @@ final class UXCodeTextView: UXTextView, HighlightDelegate, UIScrollViewDelegate 
 protocol UXCodeTextViewDelegate: UXTextViewDelegate {
     var allowCopy: Bool { get }
     var fontSize: CGFloat? { get set }
-
+    
     func setDragSelection(_: Range<Int>?)
 }
 
 // MARK: - Smarts as shown in https://github.com/naoty/NTYSmartTextView
 
 extension UXTextView {
-
+    
     var swiftSelectedRange: Range<String.Index> {
         let s = self.string
         guard !s.isEmpty else { return s.startIndex..<s.startIndex }
@@ -451,12 +451,12 @@ extension UXTextView {
 #endif
         return selectedRange
     }
-
+    
     fileprivate var currentLine: String {
         let s = self.string
         return String(s[s.lineRange(for: swiftSelectedRange)])
     }
-
+    
     fileprivate var isEndOfLine: Bool {
         let ( _, isEnd ) = getStartOrEndOfLine()
         return isEnd
@@ -465,7 +465,7 @@ extension UXTextView {
         let ( isStart, isEnd ) = getStartOrEndOfLine()
         return isStart || isEnd
     }
-
+    
     fileprivate func getStartOrEndOfLine() -> ( isStart: Bool, isEnd: Bool ) {
         let s             = self.string
         let selectedRange = self.swiftSelectedRange
@@ -486,12 +486,12 @@ extension NSTextView {
 }
 #else // iOS
 extension UITextView {
-
+    
     var string: String { // NeXTstep was right!
         set { text = newValue}
         get { text }
     }
-
+    
     var codeTextStorage: NSTextStorage? { textStorage }
 }
 #endif // iOS
