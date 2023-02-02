b0ae28b7ad02ebf471e5c30e3f33c1733a8bf5b7
Katjana Kosic <ge56yuw@tum.de>
2023-02-02 18:45:53 +0100

    Add line behind calendars

--- Themis/Views/Custom/DateTimelineView.swift
+++ Themis/Views/Custom/DateTimelineView.swift
@@ -10,23 +10,29 @@ import SwiftUI
  struct DateTimelineView: View {
     let dates: [(name: String, date: String?)]
 
-    var body: some View {
-        HStack {
-            ForEach(dates, id: \.name) { date in
-                VStack {
-                    Text(date.name)
-                    Image(systemName: "calendar")
-                        .font(.system(size: 30))
-                        .padding(1)
-                    if let dateString = ArtemisDateHelpers.getRemainingOrOverdueTime(for: date.date) {
-                        Text(dateString)
-                    } else {
-                        Text("not available yet")
-                    }
-                }
-                .frame(maxWidth: 150)
-                .padding()
-            }
-        }
+     var body: some View {
+         ZStack(alignment: .center) {
+             Rectangle()
+                 .frame(width: 650, height: 3)
+                 .foregroundColor(Color.blue)
+             HStack {
+                 ForEach(dates, id: \.name) { date in
+                     VStack {
+                         Text(date.name)
+                         Image(systemName: "calendar")
+                             .background(Color.white)
+                             .font(.system(size: 30))
+                             .padding(1)
+                         if let dateString = ArtemisDateHelpers.getRemainingOrOverdueTime(for: date.date) {
+                             Text(dateString)
+                         } else {
+                             Text("not available yet")
+                         }
+                     }
+                     .frame(maxWidth: 150)
+                     .padding()
+                 }
+             }
+         }
     }
  }
