1c5bf4748d223cf9b1a587d58a00afcf7543f2b1
Katjana Kosic <ge56yuw@tum.de>
2023-02-03 11:16:05 +0100

    Add dismiss button

--- Themis/Views/Custom/DateTimelineView.swift
+++ Themis/Views/Custom/DateTimelineView.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 struct DateTimelineView: View {
     let dates: [(name: String, date: String?)]
-    @State private var showExactDate = false
-    @State private var selectedDate: String?
+    @State private var showingAlert = false
+    @State private var selectedDate: (String, String?) = ("", nil)
 
     var body: some View {
         ZStack(alignment: .center) {
@@ -25,16 +25,14 @@ struct DateTimelineView: View {
                             .background(Color.white)
                             .font(.system(size: 30))
                             .padding(1)
-                            .onLongPressGesture(minimumDuration: 3.0, maximumDistance: 10, pressing: {_ in
-                                selectedDate = date.date
-                                showExactDate.toggle()
-                            }, perform: {
-                                showExactDate = false
-                            })
+                            .onLongPressGesture {
+                                selectedDate = (date.name, date.date)
+                                showingAlert = true
+                            }
                         if let dateString = ArtemisDateHelpers.getRemainingOrOverdueTime(for: date.date) {
                             Text(dateString)
                         } else {
-                            Text("not available yet")
+                            Text("Not available yet")
                         }
                     }
                     .frame(maxWidth: 150)
@@ -42,12 +40,10 @@ struct DateTimelineView: View {
                 }
             }
         }
-        .alert(isPresented: $showExactDate) {
-            if let date = selectedDate, let dateString = ArtemisDateHelpers.getReadableDateString(date) {
-                return Alert(title: Text("Date"), message: Text(dateString), dismissButton: .default(Text("OK")))
-            } else {
-                return Alert(title: Text("Date"), message: Text("Not available yet"), dismissButton: .default(Text("OK")))
-            }
+        .alert(isPresented: $showingAlert) {
+            Alert(title: Text("Due date for \(selectedDate.0)"),
+                  message: selectedDate.1.map { date in Text(ArtemisDateHelpers.getReadableDateString(date) ?? "Not available yet") },
+                  dismissButton: nil)
         }
     }
 }
