0e2ac5ad3e3db73a770d9fd8ec6afefb8d54594d
Katjana Kosic <ge56yuw@tum.de>
2023-02-03 09:58:07 +0100

    Remove commented code

--- Themis/Views/Custom/DateTimelineView.swift
+++ Themis/Views/Custom/DateTimelineView.swift
@@ -7,35 +7,6 @@
 
 import SwiftUI
 
-// struct DateTimelineView: View {
-//    let dates: [(name: String, date: String?)]
-//
-//     var body: some View {
-//         ZStack(alignment: .center) {
-//             Rectangle()
-//                 .frame(width: 650, height: 6)
-//                 .foregroundColor(Color.blue)
-//             HStack {
-//                 ForEach(dates, id: \.name) { date in
-//                     VStack {
-//                         Text(date.name)
-//                         Image(systemName: "calendar")
-//                             .background(Color.white)
-//                             .font(.system(size: 30))
-//                             .padding(1)
-//                         if let dateString = ArtemisDateHelpers.getRemainingOrOverdueTime(for: date.date) {
-//                             Text(dateString)
-//                         } else {
-//                             Text("not available yet")
-//                         }
-//                     }
-//                     .frame(maxWidth: 150)
-//                     .padding()
-//                 }
-//             }
-//         }
-//    }
-// }
 struct DateTimelineView: View {
     let dates: [(name: String, date: String?)]
     @State private var showExactDate = false
