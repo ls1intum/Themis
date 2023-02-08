8d6d08097e4ef217284cc38cf3dfd77a0607464f
Paul Schwind <pal03377@users.noreply.github.com>
2023-02-08 16:42:57 +0100

    Mini fixes assessment view (#116)

--- Themis/Views/Assessment/AssessmentView.swift
+++ Themis/Views/Assessment/AssessmentView.swift
@@ -287,7 +287,7 @@ struct AssessmentView: View {
                 )
             }
         }
-        .task(priority: .high) {
+        .task {
             if let submissionId, vm.submission == nil {
                 await vm.getSubmission(id: submissionId)
             }
