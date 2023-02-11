aa8eda128a4c0411a90d5cb9a92d83232c22e30f
Florian Huber <florian.maximilian.huber@tum.de>
2023-02-11 13:54:56 +0100

    review changes

--- Themis/Views/Exercises/ExerciseView.swift
+++ Themis/Views/Exercises/ExerciseView.swift
@@ -27,7 +27,7 @@ struct ExerciseView: View {
                                 exercise: exercise,
                                 submissionStatus: .open
                             )
-                        }.disabled(submissionDueDateNotOver)
+                        }.disabled(!submissionDueDateOver)
                     }
                     if !submissionListVM.submittedSubmissions.isEmpty {
                         Section("Finished submissions") {
@@ -36,7 +36,7 @@ struct ExerciseView: View {
                                 exercise: exercise,
                                 submissionStatus: .submitted
                             )
-                        }.disabled(submissionDueDateNotOver)
+                        }.disabled(!submissionDueDateOver)
                     }
                     Section("Statistics") {
                         HStack(alignment: .center) {
@@ -69,7 +69,7 @@ struct ExerciseView: View {
                 }
             }
             ToolbarItem(placement: .navigationBarTrailing) {
-                startNewAssessmentButton.disabled(submissionDueDateNotOver)
+                startNewAssessmentButton.disabled(!submissionDueDateOver)
             }
         }
         .errorAlert(error: $assessmentVM.error)
@@ -100,11 +100,11 @@ struct ExerciseView: View {
         }
         .buttonStyle(NavigationBarButton())
     }
-    private var submissionDueDateNotOver: Bool {
+    private var submissionDueDateOver: Bool {
         if let dueDate = exercise.dueDate, dueDate <= ArtemisDateHelpers.stringifyDate(Date.now)! {
-            return false
+            return true
         }
-        return true
+        return false
     }
     
     private func fetchExerciseData() async {
