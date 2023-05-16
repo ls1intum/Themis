d17040acc74b8b8993315b25169f4fc7db9d855a
Tarlan Ismayilsoy <terlan_ismayilsoy@hotmail.com>
2023-05-16 16:13:31 +0400

    Integrate exercise statistics fetch calls; Move exercise statistics models to ArtemisCore

--- Themis/ViewModels/Exercise/ExerciseViewModel.swift
+++ Themis/ViewModels/Exercise/ExerciseViewModel.swift
@@ -13,8 +13,8 @@ import SharedServices
 class ExerciseViewModel: ObservableObject {
     @Published var exercise: DataState<Exercise> = .loading
     @Published var exam: Exam?
-    @Published var exerciseStats: ExerciseStatsForAssessmentDashboard?
-    @Published var exerciseStatsForDashboard: ExerciseStatistics?
+    @Published var exerciseStatsForAssessment: DataState<ExerciseStatsForAssessmentDashboard> = .loading
+    @Published var exerciseStats: DataState<ExerciseStatistics> = .loading
     @Published var error: Error?
 
     @MainActor
@@ -29,9 +29,9 @@ class ExerciseViewModel: ObservableObject {
 
     @MainActor
     func fetchExerciseStats(exerciseId: Int) async {
-        do {
-            self.exerciseStats = try await ArtemisAPI.getExerciseStats(exerciseId: exerciseId)
-        } catch let error {
+        self.exerciseStatsForAssessment = await ExerciseServiceFactory.shared.getExerciseStatsForAssessmentDashboard(exerciseId: exerciseId)
+        
+        if case .failure(let error) = exerciseStatsForAssessment {
             self.error = error
             log.error(String(describing: error))
         }
@@ -39,36 +39,36 @@ class ExerciseViewModel: ObservableObject {
     
     @MainActor
     func fetchExerciseStatsForDashboard(exerciseId: Int) async {
-        do {
-            self.exerciseStatsForDashboard = try await ArtemisAPI.getExerciseStatsForDashboard(exerciseId: exerciseId)
-        } catch let error {
+        exerciseStats = await ExerciseServiceFactory.shared.getExerciseStatsForDashboard(exerciseId: exerciseId)
+        
+        if case .failure(let error) = exerciseStats {
             self.error = error
             log.error(String(describing: error))
         }
     }
     var reviewStudents: String {
-        let totalNumberOfStudents = exerciseStats?.numberOfSubmissions?.inTime ?? -1
-        let totalNumberOfAssessments = exerciseStats?.totalNumberOfAssessments?.inTime ?? -1
+        let totalNumberOfStudents = exerciseStatsForAssessment.value?.numberOfSubmissions?.inTime ?? -1
+        let totalNumberOfAssessments = exerciseStatsForAssessment.value?.totalNumberOfAssessments?.inTime ?? -1
         let percentage = Double(totalNumberOfAssessments) / Double(totalNumberOfStudents)
         let percentageString = String(format: "%.2f", percentage)
         return "\(totalNumberOfAssessments) / \(totalNumberOfStudents) (\(percentageString))"
     }
     
     var participationRate: Double {
-        let numOfParticipations = Double(exerciseStatsForDashboard?.numberOfParticipations ?? 0)
-        let numOfStudents = Double(exerciseStatsForDashboard?.numberOfStudentsOrTeamsInCourse ?? 0)
+        let numOfParticipations = Double(exerciseStats.value?.numberOfParticipations ?? 0)
+        let numOfStudents = Double(exerciseStats.value?.numberOfStudentsOrTeamsInCourse ?? 0)
         return Double(numOfParticipations / numOfStudents)
     }
     
     var averageScore: Double {
-        let avgScore = Double(exerciseStatsForDashboard?.averageScoreOfExercise ?? 0)
-        let maxPoints = Double(exerciseStatsForDashboard?.maxPointsOfExercise ?? 0)
+        let avgScore = Double(exerciseStats.value?.averageScoreOfExercise ?? 0)
+        let maxPoints = Double(exerciseStats.value?.maxPointsOfExercise ?? 0)
         return Double(avgScore / maxPoints)
     }
     
     var assessed: Double {
-        let numOfAssessments = Double(exerciseStats?.totalNumberOfAssessments?.inTime ?? 0)
-        let numOfStudents = Double(exerciseStats?.numberOfSubmissions?.inTime ?? 0)
+        let numOfAssessments = Double(exerciseStatsForAssessment.value?.totalNumberOfAssessments?.inTime ?? 0)
+        let numOfStudents = Double(exerciseStatsForAssessment.value?.numberOfSubmissions?.inTime ?? 0)
         return Double(CGFloat(numOfAssessments / numOfStudents))
     }
     
