715e82feb2b0005a82ad15b0311f53f358805c0e
Tarlan Ismayilsoy <terlan_ismayilsoy@hotmail.com>
2023-05-16 16:25:20 +0400

    Refactor ExerciseViewModel

--- Themis/ViewModels/Exercise/ExerciseViewModel.swift
+++ Themis/ViewModels/Exercise/ExerciseViewModel.swift
@@ -16,36 +16,7 @@ class ExerciseViewModel: ObservableObject {
     @Published var exerciseStatsForAssessment: DataState<ExerciseStatsForAssessmentDashboard> = .loading
     @Published var exerciseStats: DataState<ExerciseStatistics> = .loading
     @Published var error: Error?
-
-    @MainActor
-    func fetchExercise(exerciseId: Int) async {
-        exercise = await ExerciseServiceFactory.shared.getExerciseForAssessment(exerciseId: exerciseId)
-        
-        if case .failure(let error) = exercise {
-            self.error = error
-            log.error(String(describing: error))
-        }
-    }
-
-    @MainActor
-    func fetchExerciseStats(exerciseId: Int) async {
-        self.exerciseStatsForAssessment = await ExerciseServiceFactory.shared.getExerciseStatsForAssessmentDashboard(exerciseId: exerciseId)
-        
-        if case .failure(let error) = exerciseStatsForAssessment {
-            self.error = error
-            log.error(String(describing: error))
-        }
-    }
     
-    @MainActor
-    func fetchExerciseStatsForDashboard(exerciseId: Int) async {
-        exerciseStats = await ExerciseServiceFactory.shared.getExerciseStatsForDashboard(exerciseId: exerciseId)
-        
-        if case .failure(let error) = exerciseStats {
-            self.error = error
-            log.error(String(describing: error))
-        }
-    }
     var reviewStudents: String {
         let totalNumberOfStudents = exerciseStatsForAssessment.value?.numberOfSubmissions?.inTime ?? -1
         let totalNumberOfAssessments = exerciseStatsForAssessment.value?.totalNumberOfAssessments?.inTime ?? -1
@@ -75,5 +46,30 @@ class ExerciseViewModel: ObservableObject {
     var isAssessmentPossible: Bool {
         (exercise.value?.isCurrentlyInAssessment ?? false)
         || exam?.isOver ?? false
+    }
+
+    @MainActor
+    func fetchExercise(exerciseId: Int) async {
+        exercise = await ExerciseServiceFactory.shared.getExerciseForAssessment(exerciseId: exerciseId)
+        showErrorIfFailedFetching(exercise)
+    }
+
+    @MainActor
+    func fetchExerciseStats(exerciseId: Int) async {
+        exerciseStatsForAssessment = await ExerciseServiceFactory.shared.getExerciseStatsForAssessmentDashboard(exerciseId: exerciseId)
+        showErrorIfFailedFetching(exerciseStatsForAssessment)
+    }
+    
+    @MainActor
+    func fetchExerciseStatsForDashboard(exerciseId: Int) async {
+        exerciseStats = await ExerciseServiceFactory.shared.getExerciseStatsForDashboard(exerciseId: exerciseId)
+        showErrorIfFailedFetching(exerciseStats)
+    }
+    
+    private func showErrorIfFailedFetching<T>(_ data: DataState<T>) {
+        if case .failure(let error) = data {
+            self.error = error
+            log.error(String(describing: error))
+        }
     }
 }
