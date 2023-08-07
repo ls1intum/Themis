fe26424dab07f25c42a3214561266105dfdc4d36
Tarlan Ismayilsoy <terlan_ismayilsoy@hotmail.com>
2023-08-07 11:42:08 +0400

    Enable view-only exercises

--- Themis/Views/Courses/CourseView.swift
+++ Themis/Views/Courses/CourseView.swift
@@ -33,10 +33,8 @@ struct CourseView: View {
                             ExerciseGroups(courseVM: courseVM, type: .inAssessment)
                                 .padding(.bottom)
                             ExerciseGroups(courseVM: courseVM, type: .viewOnly)
-                                .disabled(true) // TODO: remove once view-only mode is fully implemented
                         }
-                        .padding(.horizontal, 20)
-                        .padding(.vertical, 20)
+                        .padding(20)
                     }
                     .environmentObject(courseVM)
                     .refreshable {
