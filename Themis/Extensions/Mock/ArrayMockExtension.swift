54e9c3a11ad3917b9eb98257ea807c9eab6a3adb
Tarlan Ismayilsoy <terlan_ismayilsoy@hotmail.com>
2023-08-13 23:04:23 +0400

    Refactor mock array extension

--- Themis/Extensions/Mock/ArrayMockExtension.swift
+++ Themis/Extensions/Mock/ArrayMockExtension.swift
@@ -7,45 +7,38 @@
 
 import Foundation
 import SharedModels
+import Common
 
-// TODO: try to combine these 2 extensions into a more general one
-
-extension Array where Element == Exercise {
+extension Array {
     /// - Parameters:
     ///   - condition: the condition to be fullfilled for the array to be filled with mock data
     ///   - mockElementCount: the number of mock elements (random if not specified)
     /// - Returns: an instance filled with mock data if the specified condition is true
-    func mock(if condition: @autoclosure () -> Bool, mockElementCount: Int? = nil) -> [Exercise] {
+    func mock(if condition: @autoclosure () -> Bool, mockElementCount: Int? = nil) -> Self {
         guard condition() else {
             return self
         }
-        var mockArray = [Exercise]()
+        
+        var mockArray = [Element]()
+        var elementToBeCloned: Element?
+        
+        switch self {
+        case is [Exercise]:
+            elementToBeCloned = Exercise.mockText as? Element
+        case is [Exam]:
+            elementToBeCloned = Exam.mock as? Element
+        default:
+            log.debug("Could not generate mock for type: \(type(of: self))")
+        }
         
         var elementCount = mockElementCount ?? Int.random(in: 1...3)
         
-        for _ in 0..<elementCount {
-            mockArray.append(.mockText)
-        }
-        
-        return mockArray
-    }
-}
-
-extension Array where Element == Exam {
-    /// - Parameters:
-    ///   - condition: the condition to be fullfilled for the array to be filled with mock data
-    ///   - mockElementCount: the number of mock elements (random if not specified)
-    /// - Returns: an instance filled with mock data if the specified condition is true
-    func mock(if condition: @autoclosure () -> Bool, mockElementCount: Int? = nil) -> [Exam] {
-        guard condition() else {
-            return self
-        }
-        var mockArray = [Exam]()
-        
-        var elementCount = mockElementCount ?? Int.random(in: 1...3)
-        
-        for _ in 0..<elementCount {
-            mockArray.append(.mock)
+        if let elementToBeCloned {
+            for _ in 0..<elementCount {
+                mockArray.append(elementToBeCloned)
+            }
+        } else {
+            log.debug("Could not generate a correct mock value for type: \(type(of: self))")
         }
         
         return mockArray
