9198b5f3739ca0aed2e10ee11dd670173a6663ce
Tarlan Ismayilsoy <terlan_ismayilsoy@hotmail.com>
2023-10-16 00:00:26 +0400

    Change the toolbar cancel button for assessed submissions

--- Themis/Views/Assessment/Toolbar/ToolbarCancelButton.swift
+++ Themis/Views/Assessment/Toolbar/ToolbarCancelButton.swift
@@ -13,11 +13,46 @@ struct ToolbarCancelButton: View {
     @Binding var presentationMode: PresentationMode
     @State private var showCancelDialog = false
     
+    /// Indicates that the user is viewing a submission that was previously assessed and submitted.
+    /// In this case, they can override the assessment, but can not cancel it.
+    private var submissionIsAssessed: Bool {
+        assessmentVM.submission?.isAssessed == true
+    }
+    
+    private var saveButtonLabel: String {
+        submissionIsAssessed ? "Override" : "Save"
+    }
+    
+    private var saveButtonRole: ButtonRole? {
+        submissionIsAssessed ? ButtonRole.destructive : nil
+    }
+    
+    private var deleteButtonLabel: String {
+        submissionIsAssessed ? "Discard" : "Delete"
+    }
+    
+    private var deleteButtonRole: ButtonRole? {
+        submissionIsAssessed ? nil : ButtonRole.destructive
+    }
+    
+    private var message: String {
+        if submissionIsAssessed {
+            """
+            Either discard your recent changes \
+            or override the existing assessment.
+            """
+        } else {
+            """
+            Either discard the assessment \
+            and release the lock (recommended) \
+            or keep the lock and save the assessment without submitting it.
+            """
+        }
+    }
+    
     var body: some View {
         Button {
-            Task {
-                assessmentVM.readOnly ? presentationMode.dismiss() : showCancelDialog.toggle()
-            }
+            assessmentVM.readOnly ? presentationMode.dismiss() : showCancelDialog.toggle()
         } label: {
             HStack {
                 Image(systemName: "chevron.left")
@@ -26,25 +61,23 @@ struct ToolbarCancelButton: View {
             .foregroundColor(.white)
         }
         .confirmationDialog("Cancel Assessment", isPresented: $showCancelDialog) {
-            Button("Save") {
+            Button(saveButtonLabel, role: saveButtonRole) {
                 Task {
                     await assessmentVM.saveAssessment()
                     presentationMode.dismiss()
                 }
             }
             
-            Button("Delete", role: .destructive) {
+            Button(deleteButtonLabel, role: deleteButtonRole) {
                 Task {
-                    await assessmentVM.cancelAssessment()
+                    if !submissionIsAssessed {
+                        await assessmentVM.cancelAssessment()
+                    }
                     presentationMode.dismiss()
                 }
             }
         } message: {
-            Text("""
-                 Either discard the assessment \
-                 and release the lock (recommended) \
-                 or keep the lock and save the assessment without submitting it.
-                 """)
+            Text(message)
         }
     }
 }
