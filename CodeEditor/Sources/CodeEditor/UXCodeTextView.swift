2c9d519eb0f5c047815d3e6ed0265d8e38b14908
Tarlan Ismayilsoy <terlan_ismayilsoy@hotmail.com>
2023-10-16 22:44:55 +0400

    Remove unnecessary field and add a check for empty space selection

--- CodeEditor/Sources/CodeEditor/UXCodeTextView.swift
+++ CodeEditor/Sources/CodeEditor/UXCodeTextView.swift
@@ -79,7 +79,6 @@ final class UXCodeTextView: UXTextView, HighlightDelegate, UIScrollViewDelegate 
     }
     
     var selectionGranularity = UITextGranularity.character
-    var canSelectionIncludeHighlightedRanges = true
     
     var feedbackMode = true
     
@@ -522,11 +521,18 @@ final class UXCodeTextView: UXTextView, HighlightDelegate, UIScrollViewDelegate 
     
     /// Validates the `dragSelection` value
     private func isSelectionValid() -> Bool {
-        guard !canSelectionIncludeHighlightedRanges else { return true }
+        guard let dragSelection else { return false }
         
+        // Check for empty selection
+        if let selectedRangeAsNSRange = Range(dragSelection.toNSRange(), in: self.text),
+           string[selectedRangeAsNSRange].trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+            return false
+        }
+        
+        // Check for overlap
         let highlightedIntRanges = highlightedRanges.compactMap({ Range($0.range) })
         for range in highlightedIntRanges {
-            if self.dragSelection?.overlaps(range) == true {
+            if dragSelection.overlaps(range) == true {
                 return false
             }
         }
