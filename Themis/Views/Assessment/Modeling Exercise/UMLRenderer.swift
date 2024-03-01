bf24b9f5eaf2b799c98b0210680631583c0a00b7
Alexander Görtzen <40467337+AlexanderG2207@users.noreply.github.com>
2024-03-01 18:06:04 +0100

    Use background from ApollonView and add message for no example solution

--- Themis/Views/Assessment/Modeling Exercise/UMLRenderer.swift
+++ Themis/Views/Assessment/Modeling Exercise/UMLRenderer.swift
@@ -14,122 +14,36 @@ import ApollonShared
 struct UMLRenderer: View {
     @ObservedObject var umlRendererVM: UMLRendererViewModel
 
-    @State var showResetButton = true
-    @State var scale: CGFloat = 1
-    
-    @State private var progressingScale: CGFloat = 1
-    @State private var startDragLocation = CGPoint.zero
-    @State private var dragStarted = true
-
-    /// The minimum scale value that the UML model can be scaled down to
-    private let minScale = 0.1
-    
     var body: some View {
-        ZStack(alignment: .topLeading) {
-            /// Render the Grid Background from the Apollon Package
-            GridBackgroundView(gridBackgroundViewModel: GridBackgroundViewModel())
-            Group {
-                /// If the UML Model is set, create an ApollonView View
-                if let model = umlRendererVM.umlModel, let type = model.type {
-                    ApollonView(umlModel: model,
-                                diagramType: type,
-                                fontSize: umlRendererVM.fontSize,
-                                themeColor: Color.Artemis.artemisBlue,
-                                diagramOffset: umlRendererVM.offset,
-                                isGridBackground: false) {}
-                }
-                Canvas(rendersAsynchronously: true) { context, size in
-                    umlRendererVM.renderHighlights(&context, size: size)
-                } symbols: {
-                    umlRendererVM.generatePossibleSymbols()
-                }
-                .onTapGesture { tapLocation in
-                    umlRendererVM.selectItem(at: tapLocation)
-                }
-            }
-            .frame(minWidth: umlRendererVM.diagramSize.width,
-                   minHeight: umlRendererVM.diagramSize.height)
-            .padding()
-            .scaleEffect(scale * progressingScale)
-            .position(umlRendererVM.currentDragLocation)
-            
-            resetZoomAndLocationButton
-        }
-        .onChange(of: umlRendererVM.diagramSize) {
-            umlRendererVM.setDragLocation()
-        }
-        .gesture(
-            DragGesture()
-                .onChanged(handleDrag)
-                .onEnded { _ in
-                    dragStarted = true
-                }
-        )
-        .simultaneousGesture(
-            MagnificationGesture()
-                .onChanged(handleMagnification)
-                .onEnded(handleMagnificationEnd)
-        )
-    }
-    
-    @ViewBuilder
-    private var resetZoomAndLocationButton: some View {
-        if showResetButton {
-            Button {
-                umlRendererVM.setDragLocation()
-                scale = 1
-                progressingScale = 1
-            } label: {
-                Image(systemName: "scope")
-                    .frame(alignment: .topLeading)
-                    .foregroundColor(.white)
-                    .padding(5)
-                    .background {
-                        RoundedRectangle(cornerRadius: 5)
-                            .foregroundColor(.themisSecondary)
+        ZStack {
+            if let model = umlRendererVM.umlModel, let type = model.type {
+                ApollonView(umlModel: model,
+                            diagramType: type,
+                            fontSize: umlRendererVM.fontSize,
+                            themeColor: Color.Artemis.artemisBlue,
+                            diagramOffset: umlRendererVM.offset,
+                            isGridBackground: true) {
+                    Canvas(rendersAsynchronously: true) { context, size in
+                        umlRendererVM.renderHighlights(&context, size: size)
+                    } symbols: {
+                        umlRendererVM.generatePossibleSymbols()
                     }
+                    .onTapGesture { tapLocation in
+                        umlRendererVM.selectItem(at: tapLocation)
+                    }
+                }
             }
-            .padding(12)
-        }
-    }
-    
-    private func handleDrag(_ gesture: DragGesture.Value) {
-        if dragStarted {
-            dragStarted = false
-            startDragLocation = umlRendererVM.currentDragLocation
-        }
-        umlRendererVM.setDragLocation(at: CGPoint(x: startDragLocation.x + gesture.translation.width,
-                                                  y: startDragLocation.y + gesture.translation.height))
-    }
-    
-    private func handleMagnification(_ newScale: MagnificationGesture.Value) {
-        progressingScale = newScale
-        
-        // Enforce zoom out limit
-        if progressingScale * scale < minScale {
-            progressingScale = minScale / scale
-        }
-    }
-    
-    private func handleMagnificationEnd(_ finalScale: MagnificationGesture.Value) {
-        scale *= finalScale
-        progressingScale = 1
-        
-        // Enforce zoom out limit
-        if scale < minScale {
-            scale = minScale
         }
     }
 }
 
 struct UMLRenderer_Previews: PreviewProvider {
     static var umlRendererVM = UMLRendererViewModel()
-    
+
     static var previews: some View {
         UMLRenderer(umlRendererVM: umlRendererVM)
             .onAppear {
                 umlRendererVM.setup(basedOn: Submission.mockModeling.baseSubmission, AssessmentResult())
             }
-            .padding()
     }
 }
